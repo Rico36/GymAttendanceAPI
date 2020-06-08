9a55fc532cfa6635ace06c38ea54ba2851c371f2
Rico36 <ricky.freyre@gmail.com>
2020-06-08 13:03:52 -0400

    Initial

---
+++ routes/users.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var router = express.Router();
+// Global variables
+// Request path module for relative path
+const path = require('path')
+// Request File System Module
+var fs = require('fs');
+
+// Read the entore list of regstered users into memory
+var users =  JSON.parse(fs.readFileSync('./data/MOCK_DATA.json', 'utf8'));
+
+// '''''''''''''''''''''''''''''''''''''''
+// GET request for the /list_user page.
+// '''''''''''''''''''''''''''''''''''''''
+router.get('/', function (req, res) {
+
+    console.log("Got a GET request for list of users");
+    
+    /* Just send the file as JSON*/
+    res.send(users);
+  });  
+
+
+
+// '''''''''''''''''''''''''''''''''''''''
+// GET record by HHS-ID .
+// '''''''''''''''''''''''''''''''''''''''
+router.get("/hhsid/:hhsid", (req, res) => {
+    const itemId = req.params.hhsid;
+    const item = users.find(_item => _item.hhsid === itemId);
+    console.log("Got a GET request for HHS-id: "+itemId);
+ 
+    if (item) {
+       res.json(item);
+    } else {
+       res.json({ message: `item ${itemId} doesn't exist`})
+    }
+ });
+
+// '''''''''''''''''''''''''''''''''''''''
+// GET record by USER-ID .
+// '''''''''''''''''''''''''''''''''''''''
+router.get("/userid/:userid", (req, res) => {
+  const itemId = req.params.userid;
+  const item = users.find(_item => _item.userid.toLowerCase() === itemId.toLowerCase());
+  console.log("Got a GET request for user id: "+itemId);
+
+  if (item) {
+     res.json(item);
+  } else {
+     res.json({ message: `item ${itemId} doesn't exist`})
+  }
+});
+
+// '''''''''''''''''''''''''''''''''''''''
+// POST check-in data.
+// '''''''''''''''''''''''''''''''''''''''
+router.post("/checkinData", (request,response) => {
+   try {
+         console.log("Got a POST request with: "+ JSON.stringify(request.body));
+         response.json('message: ok');
+
+      } catch (err) { console.error('POST error: '+ err); response.json('message: '+err); }
+     
+ });
+
+
+module.exports = router;
