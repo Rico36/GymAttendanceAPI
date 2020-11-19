7ec1234e504597e64f67316880d00fa4fb5a58c5
Rico36 <ricky.freyre@gmail.com>
2020-11-18 19:20:21 -0500

    Final

--- restartServer.sh
+++ restartServer.sh
@@ -1,6 +1,8 @@
 
 # STOP the web server 
 cd /opt/FitnessCenterSrv
+sudo systemctl quit nginx
+sudo service smbd stop
 sudo pm2 stop app.js
 
 ## Download all code changes from GIT and ignore/discard any local changes (if any)
@@ -8,7 +10,10 @@ sudo pm2 stop app.js
 # sudo git reset --hard origin/master
 
 #run the web server app as a background process
+sudo nginx -t
+sudo systemctl start nginx
 sudo pm2 start app.js --watch --ignore-watch="[BAK, MembersData]" --name="Fitness Center App"
 sudo pm2 startup systemd
 sudo pm2 status
 #
+sudo service smbd restart
---
+++ serverStatus.sh
@@ -0,0 +1,4 @@
+## SHows the multiple components running status
+systemctl status nginx
+sudo pm2 status
+#
