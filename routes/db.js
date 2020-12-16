10702bdfeac7f224e30e80a38e2225d389ce5c5f
Rico36 <ricky.freyre@gmail.com>
2020-12-16 17:38:11 -0500

    Final

--- routes/db.js
+++ routes/db.js
@@ -8,45 +8,45 @@ const express = require('express'),
     UserControls = require("../controllers/UserController.js");
 
 // Get All 
-router.get("/", function (req, res) {
+router.get("/", isLoggedIn, function (req, res) {
     console.log ("db/");
     res.json( {message:  "ok"});
 });
 
 router.get("/checkins", CheckinControls.all);
 
-router.post("/members/create", MemberControls.create);
-router.put("/members/update", MemberControls.update);
-router.delete("/members/delete", MemberControls.delete);
-router.get("/members", MemberControls.all);
-router.get("/members/:userid", MemberControls.find);
-router.get("/members/activate/:userid", MemberControls.activate);
-router.get("/members/deactivate/:userid", MemberControls.deactivate);
-router.get("/members/:hhsid", MemberControls.update);
+router.post("/members/create", isLoggedIn, MemberControls.create);
+router.put("/members/update", isLoggedIn, MemberControls.update);
+router.delete("/members/delete", isLoggedIn, MemberControls.delete);
+router.get("/members", isLoggedIn, MemberControls.all);
+router.get("/members/:userid", isLoggedIn, MemberControls.find);
+router.get("/members/activate/:userid", isLoggedIn, MemberControls.activate);
+router.get("/members/deactivate/:userid", isLoggedIn, MemberControls.deactivate);
+router.get("/members/:hhsid", isLoggedIn, MemberControls.update);
 
-router.post("/rooms/create", RoomControls.create);
+router.post("/rooms/create", isLoggedIn, RoomControls.create);
 router.put("/rooms/update", RoomControls.update);
-router.delete("/rooms/delete", RoomControls.delete);
-router.get("/rooms", RoomControls.all);
-router.get("/rooms/:rm", RoomControls.find);
-router.get("/rooms/activate/:rm", RoomControls.activate);
-router.get("/rooms/deactivate/:rm", RoomControls.deactivate);
+router.delete("/rooms/delete", isLoggedIn, RoomControls.delete);
+router.get("/rooms", isLoggedIn, RoomControls.all);
+router.get("/rooms/:rm", isLoggedIn, RoomControls.find);
+router.get("/rooms/activate/:rm", isLoggedIn, RoomControls.activate);
+router.get("/rooms/deactivate/:rm", isLoggedIn, RoomControls.deactivate);
 
-router.post("/devices/create", DeviceControls.create);
+router.post("/devices/create", isLoggedIn, DeviceControls.create);
 router.put("/devices/update", DeviceControls.update);
-router.delete("/devices/delete", DeviceControls.delete);
+router.delete("/devices/delete", isLoggedIn, DeviceControls.delete);
 router.get("/devices", DeviceControls.all);
 router.get("/devices/:rm", DeviceControls.find);
-router.get("/devices/activate/:rm", DeviceControls.activate);
-router.get("/devices/deactivate/:rm", DeviceControls.deactivate);
+router.get("/devices/activate/:rm", isLoggedIn, DeviceControls.activate);
+router.get("/devices/deactivate/:rm", isLoggedIn,  DeviceControls.deactivate);
 
-router.post("/users/create", UserControls.create);
-router.put("/users/update", UserControls.update);
-router.delete("/users/delete", UserControls.delete);
-router.get("/users", UserControls.all);
-router.get("/users/:email", UserControls.find);
-router.get("/users/activate/:email", UserControls.activate);
-router.get("/users/deactivate/:email", UserControls.deactivate);
+router.post("/users/create", isLoggedIn, UserControls.create);
+router.put("/users/update", isLoggedIn, UserControls.update);
+router.delete("/users/delete", isLoggedIn,  UserControls.delete);
+router.get("/users", isLoggedIn, UserControls.all);
+router.get("/users/:email", isLoggedIn, UserControls.find);
+router.get("/users/activate/:email", isLoggedIn, UserControls.activate);
+router.get("/users/deactivate/:email", isLoggedIn,  UserControls.deactivate);
 
 router.get("/reports").post((req, res, next) => {
   var startDate = moment(req.body.currentDate)
@@ -78,5 +78,13 @@ router.get("/reports").post((req, res, next) => {
         }
     })
 })
+
+/* check if user is logged in */ 
+function isLoggedIn(req, res, next) { 
+    if (req.isAuthenticated()) 
+        return next(); 
+    
+    res.status(403).send("access denied");
+  } 
 
 module.exports = router;
